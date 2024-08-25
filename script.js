bd84c4ceab622b20d9d0a5d75c4dc16bccb300c0
sahil Bisht <sahilbisht036@gmail.com>
2024-08-25 06:28:16 +0530

    first commit

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//     const textElement = document.querySelector('.typing-text');
+//     const text = textElement.textContent;
+//     textElement.textContent = '';
+//     let index = 0;
+
+//     function type() {
+//       if (index < text.length) {
+//         textElement.textContent += text.charAt(index);
+//         index++;
+//         setTimeout(type, 100); // Adjust typing speed here
+//       }
+//     }
+
+//     type();
+//   });
 document.addEventListener("DOMContentLoaded", function() {
     const textElement = document.querySelector('.typing-text');
     const text = textElement.textContent;
-    textElement.textContent = '';
+    textElement.textContent = ''; // Clear the text initially
     let index = 0;
-
+  
     function type() {
       if (index < text.length) {
         textElement.textContent += text.charAt(index);
         index++;
         setTimeout(type, 100); // Adjust typing speed here
+      } else {
+        setTimeout(() => {
+          textElement.textContent = ''; // Clear the text
+          index = 0; // Reset index to restart typing
+          type(); // Restart typing
+        }, 1000); // Adjust the delay before restarting the typing
       }
     }
-
+  
     type();
   });
+  
