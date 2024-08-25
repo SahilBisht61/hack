32b49d4b0eea84ed985617b263b69325087bb0a2
sahil Bisht <sahilbisht036@gmail.com>
2024-08-25 09:41:36 +0530

    first commit

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     const textElement = document.querySelector('.typing-text');
-//     const text = textElement.textContent;
-//     textElement.textContent = '';
-//     let index = 0;
 
-//     function type() {
-//       if (index < text.length) {
-//         textElement.textContent += text.charAt(index);
-//         index++;
-//         setTimeout(type, 100); // Adjust typing speed here
-//       }
-//     }
-
-//     type();
-//   });
 document.addEventListener("DOMContentLoaded", function() {
     const textElement = document.querySelector('.typing-text');
     const text = textElement.textContent;
